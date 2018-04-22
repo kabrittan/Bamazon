44252789710c37853738753bcbdd635ed621ce2e
Kyle Brittan <kabrittan@gmail.com>
2018-04-22 14:30:33 -0700

    Added Seeds

---
+++ seeds.sql
@@ -0,0 +1,13 @@
+insert into products(product_name, department_name, product_price, stock_quantity)
+values ("Allegra", "Allergy Relief", 17.99, 15),
+		("Tylenol", "Pain Relief", 14.99, 10),
+        ("Tums", "Digestive Health", 6.99, 8),
+        ("Centrum", "Multivitamins", 13.99, 20),
+        ("KY Jelly", "Sexual Wellness", 19.99, 5),
+        ("Fist Aid Kit", "First Aid", 17.99, 14),
+        ("Nyquil", "Cold & Flu", 15.99, 18),
+        ("Sudafed", "Decongestants", 22.99, 12),
+        ("Nicotine Patches", "Smoking Aids", 39.99, 5),
+        ("Visine", "Eye Care", 6.99, 10);
+        
+SELECT * FROM bamazon_db.products;
